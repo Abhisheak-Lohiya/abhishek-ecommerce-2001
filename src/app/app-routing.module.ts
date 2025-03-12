641900ef9785d407f7644dfc420a2c04d1de5380
Abhisheak-Lohiya <abhisheaklohiya@gmail.com>
2025-03-12 23:18:31 +0530

    login and register page with thier routings

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './public/components/login/login.component';
+import { HomeComponent } from './public/components/home/home.component';
+import { RegisterComponent } from './public/components/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent }, 
+  { path: 'login', component: LoginComponent },  
+  { path: 'register', component: RegisterComponent },  
+  
+  { path: '**', redirectTo: '' }     
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,38 @@
+// import { NgModule } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
+
+// import { AppRoutingModule } from './app-routing.module';
+// import { AppComponent } from './app.component';
+// import { HeaderComponent } from './public/components/header/header.component';
+// import { SectionComponent } from './public/components/section/section.component';
+// import { FooterComponent } from './public/components/footer/footer.component';
+// import { LoginComponent } from './public/components/login/login.component';
+// import { RegisterComponent } from './public/components/register/register.component';
+
+// @NgModule({
+//   declarations: [
+  
+//     HeaderComponent,
+//     SectionComponent,
+//     FooterComponent,
+//     LoginComponent,
+//     RegisterComponent
+//   ],
+//   imports: [
+//     BrowserModule,
+//     AppRoutingModule,
+//     AppComponent,
+//   ],
+//   providers: [],
+//   bootstrap: [AppComponent]
+// })
+// export class AppModule { }
+
+
+// app.module.ts
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './public/components/header/header.component';
@@ -8,6 +40,7 @@ import { SectionComponent } from './public/components/section/section.component'
 import { FooterComponent } from './public/components/footer/footer.component';
 import { LoginComponent } from './public/components/login/login.component';
 import { RegisterComponent } from './public/components/register/register.component';
+import { HomeComponent } from './public/components/home/home.component';
 
 @NgModule({
   declarations: [
@@ -16,11 +49,13 @@ import { RegisterComponent } from './public/components/register/register.compone
     SectionComponent,
     FooterComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    HomeComponent, // Added this as it was missing
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule, // Ensure RouterModule is imported here
   ],
   providers: [],
   bootstrap: [AppComponent]
