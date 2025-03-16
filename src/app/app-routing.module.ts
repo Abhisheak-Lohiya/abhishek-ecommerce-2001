cbe9bab689658703268186486db838cf94fbfc83
Abhisheak-Lohiya <abhisheaklohiya@gmail.com>
2025-03-16 21:54:43 +0530

    my-profile

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './public/components/login/login.component';
 import { HomeComponent } from './public/components/home/home.component';
 import { RegisterComponent } from './public/components/register/register.component';
+import { MyProfileComponent } from './public/components/my-profile/my-profile.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent }, 
   { path: 'login', component: LoginComponent },  
-  { path: 'register', component: RegisterComponent },  
+  { path: 'register', component: RegisterComponent }, 
+  {path:'my-profile', component:MyProfileComponent} ,
   
   { path: '**', redirectTo: '' }     
 ];
